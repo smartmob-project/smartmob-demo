ecda1c572be0abe7cbbf9928749b55a1193c1e8c
Andre Caron <andre.l.caron@gmail.com>
2016-05-06 23:33:51 -0400

    Adds centralized logging,

---
+++ logging/provision.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+
+import errno
+import os
+import os.path
+import re
+import socket
+import time
+import timeit
+
+try:
+    from urllib.error import URLError
+    from urllib.request import urlopen, Request
+    from urllib.parse import urljoin
+    def autoclose(x):
+        return x
+except ImportError:
+    # Python 2.7.
+    from contextlib import contextmanager
+    from urllib2 import URLError, urlopen, Request
+    from urlparse import urljoin
+    @contextmanager
+    def autoclose(x):
+        try:
+            yield x
+        finally:
+            x.close()
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+def readfile(path):
+    """Read a binary file's contents into a byte string."""
+    with open(path, 'rb') as stream:
+        return stream.read()
+
+
+def resolve_elasticsearch_url(elasticsearch_port=9200):
+    """Deduce ElasticSearch URL from ``DOCKER_HOST``."""
+    docker_host = os.environ.get('DOCKER_HOST', '')
+    if docker_host:
+        match = re.match(r'^tcp://(.+?):\d+$', docker_host)
+        docker_ip = match.group(1)
+    else:
+        docker_ip = '127.0.0.1'
+    return 'http://%s:%d' % (docker_ip, elasticsearch_port)
+
+
+def check_status(req, rep, status_codes={200, 201}):
+    """Check HTTP status against a set of expected status codes."""
+    if not isinstance(status_codes, (list, set)):
+        status_codes = [status_codes]
+    status_code = rep.getcode()
+    if status_code not in status_codes:
+        # TODO: log information about the request and the response to help
+        #       diagnostics...
+        raise Exception(
+            'HTTP %d was unexpected (expecting one of %r).' % (
+                status_code,
+                status_codes,
+            )
+        )
+
+
+def provision(elasticsearch_url, clock=timeit.default_timer, timeout=30.0):
+    """Upload logging configurations to ElasticSearch."""
+
+    # Wait until ElasticSearch is responsive.
+    #
+    # NOTE: this is just a convenience to tolerate for the ElasticSearch Docker
+    #       container to start up.  After the timeout is elapsed, proceed as
+    #       usual to get "real" diagnostics as if we didn't poll at all.
+    ref = clock()
+    while (clock() - ref) < timeout:
+        try:
+            req = Request(url=elasticsearch_url)
+            with autoclose(urlopen(req)) as rep:
+                check_status(req, rep, {200})
+                break
+        except (ConnectionResetError, URLError) as error:
+            # Even though URLError is a subclass of OSError and has an
+            # ``errno`` attribute, it seems like it's set to ``None``, so we
+            # cannot portably test for ``errno.ECONNREFUSED``...
+            time.sleep(1.0)
+
+    # Upload ElasticSearch index templates.
+    req = Request(
+        url=urljoin(elasticsearch_url, '_template/docker'),
+        data=readfile(
+            os.path.join(here, 'elasticsearch-index-templates.json')
+        ),
+    )
+    with autoclose(urlopen(req)) as rep:
+        check_status(req, rep, {200, 201})
+
+    # Upload Kibana index patterns.
+    req = Request(
+        url=urljoin(elasticsearch_url, '.kibana/index-pattern/docker-*'),
+        data=readfile(
+            os.path.join(here, 'kibana-index-patterns.json')
+        ),
+    )
+    with autoclose(urlopen(req)) as rep:
+        check_status(req, rep, {200, 201})
+
+    # Upload Kibana configuration (default index pattern, etc.).
+    req = Request(
+        url=urljoin(elasticsearch_url, '.kibana/config/4.1.6'),
+        data=readfile(
+            os.path.join(here, 'kibana-configuration.json')
+        ),
+    )
+    with autoclose(urlopen(req)) as rep:
+        check_status(req, rep, {200, 201})
+
+
+if __name__ == '__main__':
+    provision(resolve_elasticsearch_url())
--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -1,8 +1,262 @@
 # -*- coding: utf-8 -*-
 
 
+import asyncio
+import datetime
 import pytest
 import json
+
+from pyfluent.client import FluentSender
+from unittest import mock
+
+
+@pytest.mark.asyncio
+async def test_elasticsearch_index_templates(DOCKER_IP, http_client):
+    """ElasticSearch index templates are provisionned by the setup process."""
+
+    url = 'http://%s:9200/_template' % (DOCKER_IP,)
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.json()
+    mappings = body['docker']['mappings']
+    fields = mappings['_default_']['properties']
+    assert '@timestamp' in fields
+    fields = mappings['docker']['properties']
+    assert 'container_name' in fields
+    assert 'container_id' in fields
+    assert 'source' in fields
+    assert 'log' in fields
+
+
+@pytest.mark.asyncio
+async def test_elasticsearch_index_data(DOCKER_IP, http_client):
+    """ElasticSearch index templates is functionnal."""
+
+    # Grab the current date (we'll need to use it several times and we don't
+    # want to get flakey results when we execute the tests around midnight).
+    today = datetime.date.today()
+
+    # Clear the index.
+    url = 'http://%s:9200/docker-%s' % (
+        DOCKER_IP,
+        today.isoformat(),
+    )
+    async with http_client.delete(url) as resp:
+        # May get 404 if we're the first test to run, but we'll get 200 if we
+        # successfully delete the index.
+        assert resp.status in (200, 404)
+
+    # Post an event with `_type=docker`.
+    url = 'http://%s:9200/docker-%s/docker' % (
+        DOCKER_IP,
+        today.isoformat(),
+    )
+    body = json.dumps({
+        'container_name': 'a-container-name',
+        'container_id': 'a-container-id',
+        'source': 'stdout',
+        'log': 'Hello, world!',
+    })
+    async with http_client.post(url, data=body) as resp:
+        assert resp.status == 201
+        body = await resp.text()
+
+    # Wait until the record shows up in search results.
+    await asyncio.sleep(datetime.timedelta(seconds=1).total_seconds())
+
+    # Grab the record.
+    url = 'http://%s:9200/docker-%04d-%02d-%02d/docker/_search' % (
+        DOCKER_IP, today.year, today.month, today.day,
+    )
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.json()
+    assert body['hits']['total'] == 1
+    assert body['hits']['hits'][0]['_source'] == {
+        'container_name': 'a-container-name',
+        'container_id': 'a-container-id',
+        'source': 'stdout',
+        'log': 'Hello, world!',
+    }
+
+    # Grab index stats, check that index exists and that we have our data.
+    url = 'http://%s:9200/_cat/indices?v' % (DOCKER_IP,)
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.text()
+    print('STATS:')
+    print(body)
+    print('------')
+    lines = body.split('\n')
+    lines = [line.split() for line in lines if line.strip()]
+    assert len(lines) >= 2
+    stats = [dict(zip(lines[0], line)) for line in lines[1:]]
+    stats = {index['index']: index for index in stats}
+    index = stats.pop(
+        'docker-%04d-%02d-%02d' % (today.year, today.month, today.day)
+    )
+    assert int(index['docs.count']) == 1
+
+
+@pytest.mark.asyncio
+async def test_kibana_index_patterns(DOCKER_IP, http_client):
+    """Kibana index patterns are provisionned by the setup procedure."""
+
+    url = 'http://%s:9200/.kibana/index-pattern/docker-*' % (DOCKER_IP,)
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.json()
+    assert body['_type'] == 'index-pattern'
+    assert body['_source']['title'] == 'docker-*'
+    assert body['_source']['timeFieldName'] == '@timestamp'
+
+
+@pytest.mark.asyncio
+async def test_kibana_config(DOCKER_IP, http_client):
+    """Kibana preferences are provisionned by the setup procedure."""
+
+    url = 'http://%s:9200/.kibana/config/4.1.6' % (DOCKER_IP,)
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.json()
+    assert body['_type'] == 'config'
+    assert body['_source']['defaultIndex'] == 'docker-*'
+
+
+@pytest.mark.asyncio
+async def test_fluentd_http_source(DOCKER_IP, http_client):
+    """FluentD forwards HTTP records to ElasticSearch."""
+
+    # Grab the current date (we'll need to use it several times and we don't
+    # want to get flakey results when we execute the tests around midnight).
+    today = datetime.date.today()
+
+    # Clear the index.
+    url = 'http://%s:9200/docker-%s' % (
+        DOCKER_IP,
+        today.isoformat(),
+    )
+    async with http_client.delete(url) as resp:
+        # May get 404 if we're the first test to run, but we'll get 200 if we
+        # successfully delete the index.
+        assert resp.status in (200, 404)
+
+    # Post an event with a tag that matches `docker.**` rule in `fluent.conf`.
+    url = 'http://%s:8888/docker.test' % (DOCKER_IP,)
+    body = 'json=' + json.dumps({
+        'container_name': 'a-container-name',
+        'container_id': 'a-container-id',
+        'source': 'stdout',
+        'log': 'Hello, world!',
+    })
+    head = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    async with http_client.post(url, headers=head, data=body) as resp:
+        assert resp.status == 200
+        body = await resp.text()
+
+    # Wait until the record shows up in search results.
+    await asyncio.sleep(datetime.timedelta(seconds=3).total_seconds())
+
+    # Grab the record.
+    url = 'http://%s:9200/docker-%04d-%02d-%02d/docker/_search' % (
+        DOCKER_IP, today.year, today.month, today.day,
+    )
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.json()
+    assert body['hits']['total'] == 1
+    assert body['hits']['hits'][0]['_source'] == {
+        'container_name': 'a-container-name',
+        'container_id': 'a-container-id',
+        'source': 'stdout',
+        'log': 'Hello, world!',
+        '@timestamp': mock.ANY,
+    }
+
+    # Grab index stats, check that index exists and that we have our data.
+    url = 'http://%s:9200/_cat/indices?v' % (DOCKER_IP,)
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.text()
+    print('STATS:')
+    print(body)
+    print('------')
+    lines = body.split('\n')
+    lines = [line.split() for line in lines if line.strip()]
+    assert len(lines) >= 2
+    stats = [dict(zip(lines[0], line)) for line in lines[1:]]
+    stats = {index['index']: index for index in stats}
+    index = stats.pop(
+        'docker-%04d-%02d-%02d' % (today.year, today.month, today.day)
+    )
+    assert int(index['docs.count']) >= 1
+
+
+@pytest.mark.asyncio
+async def test_fluentd_forward_source(DOCKER_IP, http_client):
+    """FluentD forwards "native" records to ElasticSearch."""
+
+    # Grab the current date (we'll need to use it several times and we don't
+    # want to get flakey results when we execute the tests around midnight).
+    today = datetime.date.today()
+
+    # Clear the index.
+    url = 'http://%s:9200/docker-%s' % (
+        DOCKER_IP,
+        today.isoformat(),
+    )
+    async with http_client.delete(url) as resp:
+        # May get 404 if we're the first test to run, but we'll get 200 if we
+        # successfully delete the index.
+        assert resp.status in (200, 404)
+
+    # Post an event with a tag that matches `docker.**` rule in `fluent.conf`.
+    fluent = FluentSender(DOCKER_IP, 24224, 'docker.test')
+    fluent.send({
+        'container_name': 'a-container-name',
+        'container_id': 'a-container-id',
+        'source': 'stdout',
+        'log': 'Hello, world!',
+    })
+
+    # Wait until the record shows up in search results.
+    await asyncio.sleep(datetime.timedelta(seconds=3).total_seconds())
+
+    # Grab the record.
+    url = 'http://%s:9200/docker-%04d-%02d-%02d/docker/_search' % (
+        DOCKER_IP, today.year, today.month, today.day,
+    )
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.json()
+    assert body['hits']['total'] == 1
+    assert body['hits']['hits'][0]['_source'] == {
+        'container_name': 'a-container-name',
+        'container_id': 'a-container-id',
+        'source': 'stdout',
+        'log': 'Hello, world!',
+        '@timestamp': mock.ANY,
+    }
+
+    # Grab index stats, check that index exists and that we have our data.
+    url = 'http://%s:9200/_cat/indices?v' % (DOCKER_IP,)
+    async with http_client.get(url) as resp:
+        assert resp.status == 200
+        body = await resp.text()
+    print('STATS:')
+    print(body)
+    print('------')
+    lines = body.split('\n')
+    lines = [line.split() for line in lines if line.strip()]
+    assert len(lines) >= 2
+    stats = [dict(zip(lines[0], line)) for line in lines[1:]]
+    stats = {index['index']: index for index in stats}
+    index = stats.pop(
+        'docker-%04d-%02d-%02d' % (today.year, today.month, today.day)
+    )
+    assert int(index['docs.count']) >= 1
 
 
 @pytest.mark.asyncio
